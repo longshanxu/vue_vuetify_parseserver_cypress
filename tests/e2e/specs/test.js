4b0fd3b3335d150501832d97329ff442ceba4dd7
longshanxu <623119632@qq.com>
2020-02-27 17:46:48 +0800

    repair gitignore

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Json.Xu
  * @Date: 2019-12-21 14:51:06
- * @LastEditTime: 2020-02-27 17:06:45
+ * @LastEditTime: 2020-02-27 17:37:21
  * @LastEditors: Json.Xu
  * @Description: 
  * @FilePath: \vue_vuetify_parseserver\tests\e2e\specs\test.js
@@ -15,6 +15,6 @@ describe('足球小伙', () => {
     cy.visit('http://localhost/')
     cy.get('.v-btn--depressed').click()
     cy.get(':nth-child(1) > :nth-child(1) > .v-form > :nth-child(1) > .v-input__control > .v-input__slot').type("6666");
-    
+    //
   })
 })
